60f71add97db1016c48375e91eb7538ff591dc59
Mazid <mazidyerima2@gmail.com>
2026-07-11 19:01:46 +0000

    Added the updating of the income and the expense board

--- Expense Tracker/script.js
+++ Expense Tracker/script.js
@@ -9,7 +9,6 @@ const submitBtn = document.getElementById("button");
 const form = document.getElementById("form");
 
 form.addEventListener('submit', check_type);
-console.log(Number(balance.textContent.slice(1)));
 
 function check_type(){
     event.preventDefault();
@@ -17,17 +16,24 @@ function check_type(){
         let new_balance = Number(balance.textContent.slice(1)) + Number(amountTransaction.value);
         new_balance = `$${new_balance}`;
         balance.textContent = new_balance;
+        
+        let new_income = Number(income.textContent.slice(1)) + Number(amountTransaction.value);
+        new_income = `$${new_income}`;
+        income.textContent = new_income;
 
     }else if(typeTransaction.value === 'expense'){
         let new_balance = Number(balance.textContent.slice(1)) - Number(amountTransaction.value);
         new_balance = `$${new_balance}`;
         balance.textContent = new_balance;
+
+        let new_expense = Number(expense.textContent.slice(1)) + Number(amountTransaction.value);
+        new_expense = `$${new_expense}`;
+        expense.textContent = new_expense;
     }
     form.reset();
 
     add_history();
 
-    
 }
 
 function add_history(){
