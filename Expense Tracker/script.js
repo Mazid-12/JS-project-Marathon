e58faf4407a463d1ab230f6d1d49170b2acca379
Mazid <mazidyerima2@gmail.com>
2026-07-11 22:27:50 +0000

    Now I have the history. It was quite interesting to get the errors and work toward them.Now, I have to make the data stay

--- Expense Tracker/script.js
+++ Expense Tracker/script.js
@@ -3,7 +3,7 @@ const expense = document.getElementById("expense");
 const income = document.getElementById("income");
 const typeTransaction = document.getElementById('type');
 const history = document.getElementById('history');
-const nameTransaction = document.getElementById('nametrans');
+const nameTransaction = document.getElementById('nameTrans');
 const amountTransaction = document.getElementById("amountTrans");
 const submitBtn = document.getElementById("button");
 const form = document.getElementById("form");
@@ -20,6 +20,7 @@ function check_type(){
         let new_income = Number(income.textContent.slice(1)) + Number(amountTransaction.value);
         new_income = `$${new_income}`;
         income.textContent = new_income;
+        add_history('+')
 
     }else if(typeTransaction.value === 'expense'){
         let new_balance = Number(balance.textContent.slice(1)) - Number(amountTransaction.value);
@@ -29,13 +30,24 @@ function check_type(){
         let new_expense = Number(expense.textContent.slice(1)) + Number(amountTransaction.value);
         new_expense = `$${new_expense}`;
         expense.textContent = new_expense;
+        add_history('-')
     }
     form.reset();
 
-    add_history();
-
 }
 
-function add_history(){
+function add_history(sign){
+    const historyName = document.createElement('p');
+    const historyAmount = document.createElement('p');
+    const historyBand = document.createElement('div');
+    historyAmount.textContent = `${sign} $${amountTransaction.value}`;
+    historyName.textContent = nameTransaction.value;
+
+    historyBand.appendChild(historyName);
+    historyBand.appendChild(historyAmount);
+
+    historyBand.classList.add('transaction');
+
+    history.appendChild(historyBand);
 
 }
