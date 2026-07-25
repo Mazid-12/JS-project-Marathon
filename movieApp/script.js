76b090b30aba35f015ad4ad800c8b7c02f5561b0
Mazid <mazidyerima2@gmail.com>
2026-07-25 07:48:26 +0000

    Just finished the first part of the app. I create search foor the movie and find it. The only issue now is the error handling

---
+++ movieApp/script.js
@@ -0,0 +1,65 @@
+const form = document.querySelector("#input-form");
+const input = document.querySelector(".input");
+const card = document.querySelector(".movie-card");
+
+
+form.addEventListener("submit", async event=>{
+    event.preventDefault();
+    let movie_input = input.value;
+    movie = movie_input.replace(' ', '+').toLowerCase();
+    movie_data = await getMovie(movie);
+    displayMovie(movie_data);
+    console.log(movie_data);
+    
+});
+
+async function getMovie(title){
+    const url = `http://www.omdbapi.com/?t=${title}&apikey=cb701f24`
+    const movieData = await fetch(url);
+    return await movieData.json();
+}
+
+function displayMovie(movie_data){
+    const {Title: movie_title,
+           Year: movie_year,
+           Director: movie_director,
+           Genre: movie_genre,
+           Plot: movie_plot,
+           Actors: movie_stars,
+           Poster: movie_poster} = movie_data;
+   
+    card.style.display = 'block';
+    card.textContent = "";
+
+    const poster = document.createElement("img");
+    const basic_info = document.createElement("h3");
+    const plot = document.createElement("h4");
+    const plot_value = document.createElement("p");
+    const directed_by = document.createElement("h4");
+    const director_value = document.createElement("p");
+    const stars = document.createElement("h4");
+    const stars_value = document.createElement("p");
+
+
+    poster.setAttribute('src', movie_poster);
+    poster.setAttribute('alt', 'Movie Poster');
+    basic_info.textContent = `${movie_year}, ${movie_genre}`
+    plot.textContent = "Plot";
+    directed_by.textContent = "Directed by";
+    stars.textContent = "Stars";
+    plot_value.textContent = movie_plot;
+    director_value.textContent = movie_director;
+    stars_value.textContent = movie_stars;
+
+    poster.classList.add('poster');
+
+    card.appendChild(poster);
+    card.appendChild(basic_info);
+    card.appendChild(plot);
+    card.appendChild(plot_value)
+    card.appendChild(directed_by);
+    card.appendChild(director_value);
+    card.appendChild(stars);
+    card.appendChild(stars_value);
+
+}
