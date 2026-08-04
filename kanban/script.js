4e3a9c339700d75f12e54da0cb788834c3bcf5bb
Mazid <mazidyerima2@gmail.com>
2026-08-04 17:07:08 +0000

    I added a button for adding new task in the TO-DO card. Added a form for new task, blurring the background. Now, you can add new tasks and move it around.

--- kanban/script.js
+++ kanban/script.js
@@ -1,20 +1,29 @@
 const tasks = document.querySelectorAll('.task');
 const lists = document.querySelectorAll('.list');
+const addButton = document.querySelector('.addTask');
+const taskForm = document.querySelector('.saveTask');
+const saveCard = document.querySelector('.saveCard')
+const container = document.querySelector('.container');
+const cancelBtn = document.querySelector('.cancelBtn');
 
-for(const task of tasks){
-    task.addEventListener("dragstart", dragStart);
-    task.addEventListener("dragend", dragEnd);
-}
 
 for(const list of lists){
+    list.addEventListener('pointerdown', event=>{
+        const task = event.target.closest('.task');
+        if(task){
+            task.addEventListener("dragstart", dragStart);
+            task.addEventListener("dragend", dragEnd);
+        }
+    })
+
     list.addEventListener("dragenter", dragEnter);
     list.addEventListener("dragleave", dragLeave);
     list.addEventListener("dragover", dragOver);
     list.addEventListener("drop", drop);
-}
 
+}
 function dragStart(){
-    event.dataTransfer.setData("text/plain", this.id)
+    event.dataTransfer.setData("text/plain", this.id);
 
 }
 function dragEnd(){
@@ -22,7 +31,7 @@ function dragEnd(){
     
 }
 function dragEnter(){
-    event.target.classList.add("change")
+    this.classList.add("change")
     
 }
 function dragLeave(){
@@ -41,3 +50,34 @@ function drop(){
     this.appendChild(dragTask);
     this.classList.remove("change");
 }
+
+
+addButton.addEventListener("click", ()=>{
+    saveCard.classList.remove('hidden');
+    container.classList.add('blurred');
+
+    const taskInput = document.querySelector('#taskInput');
+    taskForm.addEventListener('submit', event=>{
+        event.preventDefault();
+        if(taskInput.value.trim() !== ''){
+            const newTask = document.createElement('div');
+            const toDo = document.querySelector('#list1');
+            newTask.draggable = 'true';
+            newTask.classList.add('task');
+
+            newTask.textContent = taskInput.value.trim();
+            newId = taskInput.value.trim().replaceAll(' ', '-');
+            newTask.id = newId;
+            toDo.appendChild(newTask);
+            saveCard.classList.add('hidden');
+            container.classList.remove('blurred');
+            taskInput.value = '';
+
+        }
+    })
+    cancelBtn.addEventListener('click', ()=>{
+        saveCard.classList.add('hidden');
+        container.classList.remove('blurred');
+        taskInput.value = '';
+    })
+})
